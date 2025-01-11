4e2703c930732d5f75f7635f88d433271963c748
ejim11 <favourejim56@gmail.com>
2025-01-11 23:04:23 +0100

    ✅ added the product item comp

---
+++ components/ProductItem.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductItem = ({
+  image,
+  title,
+  price,
+  percentOff,
+}: {
+  image: StaticImageData;
+  title: string;
+  price: number;
+  percentOff: number;
+}) => {
+  const newPrice = price - (percentOff / 100) * price;
+  return (
+    <div className="flex rounded-[1rem] overflow-hidden mb-[2.5rem] bg-[rgb(31,33,37)] sm:w-full sm:h-[20rem]">
+      <div className="w-[25rem] h-[20rem] sm:w-[16rem] sm:h-full">
+        <Image
+          src={image}
+          alt={`${title} image`}
+          priority
+          width={500}
+          height={500}
+          className="w-full h-full "
+        />
+      </div>
+      <div className="p-[1.5rem] flex flex-col flex-1">
+        <p className="capitalize font-semibold text-[1.8rem]">{title}</p>
+        <p className="mt-auto font-medium">${newPrice}</p>
+        <div className="flex text-[rgba(254,254,254,0.75)]">
+          <p className="mr-[1rem] line-through">${price}</p>
+          <p>{`(${percentOff}% off)`}</p>
+        </div>
+        <Link
+          href=""
+          className="bg-color-white w-full rounded-[0.6rem] text-center text-[rgb(16,16,16)] mt-[1rem] py-[0.5rem] font-medium"
+        >
+          Buy now
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
