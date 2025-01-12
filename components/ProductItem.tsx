7cbfc3e91dd5b2590fc5eac0f6960720aa9211e0
ejim11 <favourejim56@gmail.com>
2025-01-12 23:32:59 +0100

    ✅ changed product price layout

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -7,14 +7,14 @@ const ProductItem = ({
   image,
   title,
   price,
-  percentOff,
-}: {
+}: // percentOff,
+{
   image: StaticImageData;
   title: string;
   price: number;
-  percentOff: number;
+  percentOff?: number;
 }) => {
-  const newPrice = price - (percentOff / 100) * price;
+  // const newPrice = price - (percentOff / 100) * price;
   return (
     <div className="flex rounded-[1rem] overflow-hidden mb-[2.5rem] bg-[rgb(31,33,37)] sm:w-full sm:h-[20rem]">
       <div className="w-[25rem] h-[20rem] sm:w-[16rem] sm:h-full">
@@ -29,10 +29,10 @@ const ProductItem = ({
       </div>
       <div className="p-[1.5rem] flex flex-col flex-1">
         <p className="capitalize font-semibold text-[1.8rem]">{title}</p>
-        <p className="mt-auto font-medium">${newPrice}</p>
-        <div className="flex text-[rgba(254,254,254,0.75)]">
-          <p className="mr-[1rem] line-through">${price}</p>
-          <p>{`(${percentOff}% off)`}</p>
+        {/* <p className="mt-auto font-medium">${newPrice}</p> */}
+        <div className="flex text-[rgba(254,254,254,0.75)] mt-auto">
+          <p className="mr-[1rem]">${price}</p>
+          {/* <p>{`(${percentOff}% off)`}</p> */}
         </div>
         <Link
           href=""
