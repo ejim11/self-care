3aea82849d5fd46773de7c0d164608edda63d1db
ejim11 <favourejim56@gmail.com>
2025-01-12 23:32:25 +0100

    ✅ changed products prices

--- utils/products.ts
+++ utils/products.ts
@@ -15,31 +15,31 @@ export type Product = {
 const products: Product[] = [
   {
     title: "Self care guide",
-    price: 200,
+    price: 30,
     percentOff: 20,
     image: selfCareImg,
   },
   {
     title: "Mindfulness Challenge",
-    price: 150,
+    price: 15,
     percentOff: 20,
     image: mindfullnessImg,
   },
   {
     title: "Gratitude journal",
-    price: 100,
+    price: 20,
     percentOff: 25,
     image: gratitudeImg,
   },
   {
     title: "Emotional wellness",
-    price: 200,
+    price: 10,
     percentOff: 25,
     image: emotionalImg,
   },
   {
     title: "Stress management workbook",
-    price: 120,
+    price: 25,
     percentOff: 25,
     image: stressManImg,
   },
