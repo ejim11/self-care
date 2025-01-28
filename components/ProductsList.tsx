d0ec5dcfc45febb8444f1dc27453ca52da647aea
ejim11 <favourejim56@gmail.com>
2025-01-28 23:12:16 +0100

    ✅ added the total amount in usd

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -4,6 +4,10 @@ import React from "react";
 import ProductItem from "./ProductItem";
 
 const ProductsList = () => {
+  const totalAmount = products
+    .map((product) => product.price)
+    .reduce((acc, cur) => acc + cur, 0);
+
   return (
     <div className="mt-[3rem] flex flex-col items-center font-dmsans w-full ">
       <h1 className="mb-[2rem] text-[2rem] font-semibold">See our products</h1>
@@ -14,9 +18,17 @@ const ProductsList = () => {
             image={product.image}
             title={product.title}
             price={product.price}
-            percentOff={product.percentOff}
+            // percentOff={product.percentOff}
           />
         ))}
+        <button
+          type="button"
+          className="bg-color-white text-color-black text-[2rem] w-full py-[1rem] text-center rounded-[0.6rem] mt-[2rem] hover:bg-[#e9ecef] transition-all duration-100 ease-in"
+        >
+          Buy all products <span className="line-through">{totalAmount}</span>{" "}
+          $(
+          <span>{0.25 * totalAmount}</span>) - 75% off
+        </button>
       </div>
     </div>
   );
