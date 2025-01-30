ea6336d5230f1a8b8ae88efaa40a6c13c9f62a5b
ejim11 <favourejim56@gmail.com>
2025-01-30 17:53:04 +0100

    🎨 added slug and priceId to the product in the list

--- utils/products.ts
+++ utils/products.ts
@@ -6,42 +6,65 @@ import stressManImg from "../assets/stress-management.png";
 import { StaticImageData } from "next/image";
 
 export type Product = {
+  priceId: string;
   title: string;
   price: number;
   percentOff: number;
   image: StaticImageData;
+  slug: string;
+  detail: string;
 };
 
 const products: Product[] = [
   {
+    priceId: "price_1QmuPFRC8367g7X78DEPdSZg",
     title: "Self care guide",
-    price: 50,
+    price: 20,
     percentOff: 20,
     image: selfCareImg,
+    slug: "self-care-guide",
+    detail:
+      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
   },
   {
+    priceId: "price_1QmvfsRC8367g7X7yMXQ8hIQ",
     title: "Mindfulness Challenge",
-    price: 35,
+    price: 15,
     percentOff: 20,
     image: mindfullnessImg,
+    slug: "mindfulness-challenge",
+    detail:
+      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
   },
   {
+    priceId: "price_1QmvkMRC8367g7X7MXCXphib",
     title: "Gratitude journal",
-    price: 30,
+    price: 20,
     percentOff: 25,
     image: gratitudeImg,
+    slug: "gratitude-journal",
+    detail:
+      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
   },
   {
+    priceId: "price_1QmvmZRC8367g7X7FDsc4iPS",
     title: "Emotional wellness",
-    price: 30,
+    price: 25,
     percentOff: 25,
     image: emotionalImg,
+    slug: "emotional-wellness",
+    detail:
+      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
   },
   {
+    priceId: "price_1QmvlNRC8367g7X7wNaJLrho",
     title: "Stress management workbook",
     price: 35,
     percentOff: 25,
     image: stressManImg,
+    slug: "stress-management-workbook",
+    detail:
+      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
   },
 ];
 
