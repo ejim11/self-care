a02a6c749d089688e957d490587a57e7d211c600
ejim11 <favourejim56@gmail.com>
2025-02-26 13:44:06 +0100

    🗃️ updated products data

--- utils/products.ts
+++ utils/products.ts
@@ -1,8 +1,7 @@
-import selfCareImg from "../assets/self-care.png";
-import mindfullnessImg from "../assets/mindfullness.png";
-import gratitudeImg from "../assets/gratitude.png";
-import emotionalImg from "../assets/emotional-wellness.png";
-import stressManImg from "../assets/stress-management.png";
+import chatgptImg from "../assets/chatgpt.png";
+import generativeAIImg from "../assets/generative-ai.png";
+import highImpactGptImg from "../assets/high-impact-gpt.png";
+import ultimateGptImg from "../assets/ultimate-gpt.png";
 import { StaticImageData } from "next/image";
 
 export type Product = {
@@ -12,59 +11,63 @@ export type Product = {
   percentOff: number;
   image: StaticImageData;
   slug: string;
-  detail: string;
+  details: string[];
 };
 
 const products: Product[] = [
   {
-    priceId: "price_1QmuPFRC8367g7X78DEPdSZg",
-    title: "Self care guide",
-    price: 20,
-    percentOff: 20,
-    image: selfCareImg,
-    slug: "self-care-guide",
-    detail:
-      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
-  },
-  {
-    priceId: "price_1QmvfsRC8367g7X7yMXQ8hIQ",
-    title: "Mindfulness Challenge",
-    price: 15,
-    percentOff: 20,
-    image: mindfullnessImg,
-    slug: "mindfulness-challenge",
-    detail:
-      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
-  },
-  {
-    priceId: "price_1QmvkMRC8367g7X7MXCXphib",
-    title: "Gratitude journal",
-    price: 20,
-    percentOff: 25,
-    image: gratitudeImg,
-    slug: "gratitude-journal",
-    detail:
-      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
-  },
-  {
     priceId: "price_1QmvmZRC8367g7X7FDsc4iPS",
-    title: "Emotional wellness",
+    title: "ChatGpt Simplified: Learn, Prompt and Succeed",
     price: 25,
-    percentOff: 25,
-    image: emotionalImg,
-    slug: "emotional-wellness",
-    detail:
-      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
+    percentOff: 20,
+    image: chatgptImg,
+    slug: "chatgpt-simplified",
+    details: [
+      "Understand AI fundamentals & how ChatGPT and other AI models work",
+      "Learn the science behind AI-generated content & automation",
+      "Master the basics of prompt engineering for better AI-generated responses",
+    ],
   },
   {
     priceId: "price_1QmvlNRC8367g7X7wNaJLrho",
-    title: "Stress management workbook",
+    title: "The Ultimate Guide to Generative AI & Prompting",
     price: 35,
+    percentOff: 20,
+    image: generativeAIImg,
+    slug: "the-ultimate-guide-to-generative-ai",
+    details: [
+      "Learn how to use AI effectively",
+      "Gain access to high-quality AI prompts for various industries",
+      "Automate business workflows, marketing, and content creation",
+      "Stay ahead in AI-driven industries",
+    ],
+  },
+  {
+    priceId: "price_1QmvkMRC8367g7X7MXCXphib",
+    title:
+      "High Impact Chatgpt Prompts for Business, Creativity & Productivity",
+    price: 20,
     percentOff: 25,
-    image: stressManImg,
-    slug: "stress-management-workbook",
-    detail:
-      "In this course, you will recieve a folder with a guide that will help you create viral videos like me. This guide will help you to get into the Tiktok Creator Rewards Program so that you can potentially start earning money. This guide is a great start to becoming Tiktok/Instagram Famous!",
+    image: highImpactGptImg,
+    slug: "high-impact-chatgpt-prompts",
+    details: [
+      "Unlock advanced prompt engineering techniques",
+      "Learn prompting techniques to get the best AI responses",
+      "Avoid common mistakes and optimize AI-generated outputs",
+    ],
+  },
+  {
+    priceId: "price_1QmvfsRC8367g7X7yMXQ8hIQ",
+    title: "The Ultimate ChatGpt: Prompts Handbook",
+    price: 15,
+    percentOff: 20,
+    image: ultimateGptImg,
+    slug: "the-ultimate-chatgpt",
+    details: [
+      "A structured guide to crafting the best AI prompts for different use cases",
+      "Learn how to use ChatGPT for business, productivity, and automation",
+      "Covers content creation, AI-driven customer support, brainstorming, and chatbot training",
+    ],
   },
 ];
 
