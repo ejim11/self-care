780760583443e092325f61c0d04178488ec775d6
ejim11 <favourejim56@gmail.com>
2025-03-06 23:46:33 +0100

    ✅ modified the home page

--- components/HomeComp.tsx
+++ components/HomeComp.tsx
@@ -5,18 +5,35 @@ import { FaInstagram, FaRegCircleCheck, FaTiktok } from "react-icons/fa6";
 import ProductsList from "./ProductsList";
 import { toastError, toastSuccess } from "@/utils/toastFuncs";
 import { LuBadgeAlert } from "react-icons/lu";
+import { FaFacebookSquare, FaTwitter, FaYoutube } from "react-icons/fa";
+import WhoIsTheBundleFor from "./WhoIsTheBundleFor";
+import IndustriesCovered from "./IndustriesCovered";
+import { FaRegCopyright } from "react-icons/fa6";
+import PromptVideo from "./PromptVideo";
 
 const HomeComp = () => {
   const iconClassname = "w-[2.5rem] h-[2.5rem] text-current";
 
   const iconLinks: { icon: ReactNode; link: string }[] = [
     {
-      icon: <FaTiktok className={iconClassname} />,
-      link: "",
-    },
-    {
       icon: <FaInstagram className={iconClassname} />,
       link: "https://www.instagram.com/dextterr__?igsh=MWUxYW8wcng5cWkyZg==",
+    },
+    {
+      icon: <FaFacebookSquare className={iconClassname} />,
+      link: "https://www.facebook.com/share/1F9XkLsALe/?mibextid=wwXIfr",
+    },
+    {
+      icon: <FaTiktok className={iconClassname} />,
+      link: "https://www.tiktok.com/@dextterr._?_t=ZT-8uF06F2II70&_r=1",
+    },
+    {
+      icon: <FaTwitter className={iconClassname} />,
+      link: "https://x.com/dextterr__?s=21",
+    },
+    {
+      icon: <FaYoutube className={iconClassname} />,
+      link: "https://youtube.com/@dextterrr?si=_S0Ceju5Fg4E_8ym",
     },
   ];
 
@@ -54,25 +71,41 @@ const HomeComp = () => {
   }, []);
 
   return (
-    <main className="bg-home min-h-screen py-[5rem] px-[2rem]">
-      <section className="w-full flex flex-col items-center text-color-white">
-        <div className="w-[15rem] h-[15rem] rounded-full border flex items-center justify-center border-color-white ">
-          <p className="text-[5rem] font-bold font-dmsans">DX</p>
-        </div>
-        <h1 className="my-[2rem] text-[3rem] font-bold text-center">
-          AI Power Bundle – The Ultimate AI Prompting & Productivity Toolkit
-        </h1>
-        <p className="mb-[4rem] text-[2rem] font-semibold text-center ">
-          Master AI for Business, Content Creation & Automation – All in One
-          Exclusive Bundle!
+    <main className="bg-home min-h-screen  relative">
+      <div className="h-[7rem] border-b border-b-gray-600 flex items-center fixed top-0 left-0 right-0 bg-black ">
+        <p className="text-[3rem] font-dmsans text-white font-semibold pl-[10rem]">
+          DextterrsAILab
         </p>
-        {/* <p className="mb-[4rem] text-[2rem] font-semibold text-center px-[10rem]">
-          Transform your business with our curated AI prompt library. We help
-          startups scale efficiently with ready-to-use AI directives that
-          streamline operations and deliver professional results—no technical
-          expertise needed.
-        </p> */}
-        <div className="flex items-center">
+      </div>
+      <section className="w-full flex flex-col items-center text-color-white pb-[5rem] px-[10rem] pt-[17rem] ">
+        <div className="w-full flex justify-between">
+          <div className="w-[48%]">
+            {/* <div className="w-[15rem] h-[15rem] rounded-full border flex items-center justify-center border-color-white ">
+              <p className="text-[5rem] font-bold font-dmsans">DX</p>
+            </div> */}
+            <h1 className="my-[2rem] text-[3rem] font-bold ">
+              AI Power Bundle – The Ultimate AI Prompting & Productivity Toolkit
+            </h1>
+            <p className=" text-[2rem] font-semibold  ">
+              Master AI for Business, Content Creation & Automation – All in One
+              Exclusive Bundle!
+            </p>
+            <p className="mb-[4rem] mt-[1rem] text-[2rem] font-semibold  ">
+              Transform your business with our curated AI prompt library. We
+              help startups scale efficiently with ready-to-use AI directives
+              that streamline operations and deliver professional results—no
+              technical expertise needed.
+            </p>
+          </div>
+          <div className="w-[48%]">
+            <PromptVideo />
+          </div>
+        </div>
+
+        <WhoIsTheBundleFor />
+        <ProductsList />
+        <IndustriesCovered />
+        <div className="flex items-center justify-center">
           {iconLinks.map((link, i) => (
             <Link
               key={i}
@@ -84,7 +117,10 @@ const HomeComp = () => {
             </Link>
           ))}
         </div>
-        <ProductsList />
+        <div className="flex items-center justify-center mt-[3rem]">
+          <FaRegCopyright />
+          <p>2025 DextterrsAILab. All rights reserved.</p>
+        </div>
       </section>
     </main>
   );
