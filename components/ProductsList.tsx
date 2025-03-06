5304950c3a465560840a5a530f4bc3644a09cf7c
ejim11 <favourejim56@gmail.com>
2025-03-06 23:48:45 +0100

    💄 modified the styling of the product list

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -35,8 +35,11 @@ const ProductsList = () => {
   };
 
   return (
-    <div className="mt-[3rem] flex flex-col items-center font-dmsans w-[50%] xlg:w-[80%] xmd:w-full ">
-      <div className="flex flex-col sm:w-full">
+    <div className="mt-[10rem]  flex flex-col  font-dmsans w-full ">
+      <h2 className="font-dmsans text-[3rem] font-semibold mb-[3rem] text-center">
+        Power Bundles
+      </h2>
+      <div className="grid grid-cols-2 w-full  pb-[5rem] gap-[5rem]">
         {products.map((product: Product) => (
           <ProductItem
             key={product.title}
@@ -48,33 +51,32 @@ const ProductsList = () => {
             // percentOff={product.percentOff}
           />
         ))}
-        <button
-          onClick={buyProductHandler}
-          type="button"
-          className="bg-color-white text-color-black text-[2rem] w-full py-[1rem] text-center rounded-[0.6rem] mt-[2rem] hover:bg-[#e9ecef] transition-all duration-100 ease-in "
-        >
-          {isLoading ? (
-            <div className="w-full flex justify-center">
-              <BallTriangle
-                height={30}
-                width={30}
-                radius={5}
-                color="#000000"
-                ariaLabel="ball-triangle-loading"
-                wrapperStyle={{}}
-                wrapperClass="mx-auto"
-                visible={true}
-              />
-            </div>
-          ) : (
-            <>
-              Buy all products{" "}
-              <span className="line-through">{totalAmount}</span> $(
-              <span>{Math.round(0.74 * totalAmount)}</span>) - 25% off
-            </>
-          )}
-        </button>
       </div>
+      <button
+        onClick={buyProductHandler}
+        type="button"
+        className="bg-color-white text-color-black text-[2rem] px-[3rem] py-[1rem] w-max  text-center rounded-[0.6rem] mt-[2rem] hover:bg-[#e9ecef] transition-all duration-100 ease-in "
+      >
+        {isLoading ? (
+          <div className="w-full  flex justify-center">
+            <BallTriangle
+              height={30}
+              width={30}
+              radius={5}
+              color="#000000"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{}}
+              wrapperClass="mx-auto"
+              visible={true}
+            />
+          </div>
+        ) : (
+          <>
+            Buy all <span className="line-through">{totalAmount}</span> $(
+            <span>{Math.round(0.74 * totalAmount)}</span>) - 25% off
+          </>
+        )}
+      </button>
     </div>
   );
 };
