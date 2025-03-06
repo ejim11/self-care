19d161b3ee01d92f877ad21bacc7939e0f71abf2
ejim11 <favourejim56@gmail.com>
2025-03-06 23:44:36 +0100

    💄 extended the font family, color, screen and grid template columns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,41 +7,44 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      inter: ["var(--font-inter)"],
-      dmsans: ["var(--font-dmsans)"],
-    },
-    colors: {
-      current: "curentColor",
-      transparent: "transparent",
-      "color-white": "#ffffff",
-      "color-black": "#000000",
-    },
-    screens: {
-      "3xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-      "2xl": { max: "1380px" },
-      // => @media (max-width: 1535px) { ... }
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-      xlg: { max: "1150px" },
-      // => @media (max-width: 1150px) { ... }
-      lg: { max: "1024px" },
-      // => @media (max-width: 1023px) { ... }
-      xmd: { max: "950px" },
-      // => @media (max-width: 950px) { ... }
-      md: { max: "850px" },
-      // => @media (max-width: 850px) { ... }
-      smd: { max: "700px" },
-      // => @media (max-width: 700px) { ... }
-      sm: { max: "600px" },
-      // => @media (max-width: 600px) { ... }
-      ssm: { max: "500px" },
-      // => @media (max-width: 500px) { ... }
-    },
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        dmsans: ["var(--font-dmsans)"],
+      },
+      colors: {
+        current: "curentColor",
+        transparent: "transparent",
+        "color-white": "#ffffff",
+        "color-black": "#000000",
+      },
+      screens: {
+        "3xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+        "2xl": { max: "1380px" },
+        // => @media (max-width: 1535px) { ... }
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+        xlg: { max: "1150px" },
+        // => @media (max-width: 1150px) { ... }
+        lg: { max: "1024px" },
+        // => @media (max-width: 1023px) { ... }
+        xmd: { max: "950px" },
+        // => @media (max-width: 950px) { ... }
+        md: { max: "850px" },
+        // => @media (max-width: 850px) { ... }
+        smd: { max: "700px" },
+        // => @media (max-width: 700px) { ... }
+        sm: { max: "600px" },
+        // => @media (max-width: 600px) { ... }
+        ssm: { max: "500px" },
+        // => @media (max-width: 500px) { ... }
+      },
       backgroundImage: {
         home: "linear-gradient(rgb(16, 16, 16), rgb(16, 16, 16))",
+      },
+      gridTemplateColumns: {
+        autofit: "repeat(auto-fit, minmax(28rem, 1fr))",
       },
     },
   },
