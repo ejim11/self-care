b6eecec8b61c58fd3638efb92e623be9b6b303f4
ejim11 <favourejim56@gmail.com>
2025-03-08 15:20:33 +0100

    💄 removed the screens style config extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,34 +18,35 @@ export default {
         "color-white": "#ffffff",
         "color-black": "#000000",
       },
-      screens: {
-        "3xl": { max: "1535px" },
-        // => @media (max-width: 1535px) { ... }
-        "2xl": { max: "1380px" },
-        // => @media (max-width: 1535px) { ... }
-        xl: { max: "1279px" },
-        // => @media (max-width: 1279px) { ... }
-        xlg: { max: "1150px" },
-        // => @media (max-width: 1150px) { ... }
-        lg: { max: "1024px" },
-        // => @media (max-width: 1023px) { ... }
-        xmd: { max: "950px" },
-        // => @media (max-width: 950px) { ... }
-        md: { max: "850px" },
-        // => @media (max-width: 850px) { ... }
-        smd: { max: "700px" },
-        // => @media (max-width: 700px) { ... }
-        sm: { max: "600px" },
-        // => @media (max-width: 600px) { ... }
-        ssm: { max: "500px" },
-        // => @media (max-width: 500px) { ... }
-      },
+
       backgroundImage: {
         home: "linear-gradient(rgb(16, 16, 16), rgb(16, 16, 16))",
       },
       gridTemplateColumns: {
         autofit: "repeat(auto-fit, minmax(28rem, 1fr))",
       },
+    },
+    screens: {
+      "3xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+      "2xl": { max: "1380px" },
+      // => @media (max-width: 1535px) { ... }
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+      xlg: { max: "1150px" },
+      // => @media (max-width: 1150px) { ... }
+      lg: { max: "1024px" },
+      // => @media (max-width: 1024px) { ... }
+      xmd: { max: "950px" },
+      // => @media (max-width: 950px) { ... }
+      md: { max: "850px" },
+      // => @media (max-width: 850px) { ... }
+      smd: { max: "700px" },
+      // => @media (max-width: 700px) { ... }
+      sm: { max: "600px" },
+      // => @media (max-width: 600px) { ... }
+      ssm: { max: "500px" },
+      // => @media (max-width: 500px) { ... }
     },
   },
   plugins: [],
