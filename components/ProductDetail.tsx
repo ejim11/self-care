01288a9563596082a6308aec868c9424d276d770
ejim11 <favourejim56@gmail.com>
2025-05-08 21:10:52 +0100

    ⚰️ removed unused code

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -1,40 +1,18 @@
 "use client";
 import products from "@/utils/products";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import Image from "next/image";
 import { loadStripe } from "@stripe/stripe-js";
-import { useRouter } from "next/navigation";
-import { buyProduct } from "@/services/buyProductService";
-import { BallTriangle } from "react-loader-spinner";
 import { SiCheckio } from "react-icons/si";
 import { motion } from "framer-motion";
+import formatAmount from "@/utils/formatAmount";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "");
 
 const ProductDetail = ({ productSlug }: { productSlug: string }) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const product = products.filter((prd) => prd.slug === productSlug)[0];
-
-  const router = useRouter();
-
-  const routeToPaymentPageHandler = (url: string) => {
-    router.push(url);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const buyProductHandler = async () => {
-    setIsLoading(true);
-    try {
-      await buyProduct(product.priceId, routeToPaymentPageHandler);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  };
 
   useEffect(() => {
     window.scrollTo({ top: -90, behavior: "smooth" });
@@ -124,30 +102,14 @@ const ProductDetail = ({ productSlug }: { productSlug: string }) => {
         <div className="w-[50rem] xmd:w-full xmd:order-1 xmd:mb-[3rem] xmd:relative xmd:top-0  sticky top-[4rem] border rounded-[0.6rem] border-[#c3bebe] h-max p-[1.5rem]">
           <p className="text-[3rem] font-bold">{product.title}</p>
           <p className="text-[2.5rem] font-semibold">
-            ${product.price.toFixed(2)}
+            N{formatAmount(String(product.price))}
           </p>
           <Link
-            // onClick={buyProductHandler}
             target="blank"
             href={product.paymentLink}
             className="w-full flex justify-center bg-gray-400 text-color-black py-[1rem] font-semibold text-[1.8rem] rounded-[0.6rem] mt-[2rem]"
           >
-            {isLoading ? (
-              <div className="w-full flex justify-center">
-                <BallTriangle
-                  height={30}
-                  width={30}
-                  radius={5}
-                  color="#000000"
-                  ariaLabel="ball-triangle-loading"
-                  wrapperStyle={{}}
-                  wrapperClass="mx-auto"
-                  visible={true}
-                />
-              </div>
-            ) : (
-              "Buy now"
-            )}
+            Buy now
           </Link>
         </div>
       </div>
