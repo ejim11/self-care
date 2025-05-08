1b8b74e39b6a170cd5b981be3531323793c266a3
ejim11 <favourejim56@gmail.com>
2025-05-08 21:11:38 +0100

    ⚰️ removed dead code

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -1,40 +1,14 @@
 "use client";
 import products, { Product } from "@/utils/products";
-import React, { useState } from "react";
+import React from "react";
 import ProductItem from "./ProductItem";
-import { useRouter } from "next/navigation";
-import { buyProduct } from "@/services/buyProductService";
-import { BallTriangle } from "react-loader-spinner";
 import Link from "next/link";
+import formatAmount from "@/utils/formatAmount";
 
 const ProductsList = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const totalAmount = products
     .map((product) => product.price)
     .reduce((acc, cur) => acc + cur, 0);
-
-  const router = useRouter();
-
-  const routeToPaymentPageHandler = (url: string) => {
-    router.push(url);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const buyProductHandler = async () => {
-    setIsLoading(true);
-    try {
-      await buyProduct(
-        "price_1Qmw25RC8367g7X7SV72BXyP",
-        routeToPaymentPageHandler
-      );
-
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err);
-      setIsLoading(false);
-    }
-  };
 
   return (
     <div className="mt-[10rem]  flex flex-col  font-dmsans w-full ">
@@ -56,31 +30,16 @@ const ProductsList = () => {
         ))}
       </div>
       <Link
-        href={"https://sandbox.flutterwave.com/pay/k75lhcghkz23"}
-        // onClick={buyProductHandler}
+        href={"https://paystack.com/buy/all-products-ryxeqx"}
         target="blank"
-        // type="button"
         className="bg-color-white text-color-black text-[2rem] px-[3rem] py-[1rem] w-max xmd:w-full  text-center rounded-[0.6rem] mt-[2rem] hover:bg-[#e9ecef] transition-all duration-100 ease-in "
       >
-        {isLoading ? (
-          <div className="w-full  flex justify-center">
-            <BallTriangle
-              height={30}
-              width={30}
-              radius={5}
-              color="#000000"
-              ariaLabel="ball-triangle-loading"
-              wrapperStyle={{}}
-              wrapperClass="mx-auto"
-              visible={true}
-            />
-          </div>
-        ) : (
-          <>
-            Buy all <span className="line-through">${totalAmount}</span> (
-            <span>${Math.round(0.74 * totalAmount)}</span>) - 25% off
-          </>
-        )}
+        Buy all
+        <span className="line-through">
+          N{formatAmount(String(totalAmount))}
+        </span>
+        (<span>N{formatAmount(String(Math.round(0.75 * totalAmount)))}</span>) -
+        25% off
       </Link>
     </div>
   );
