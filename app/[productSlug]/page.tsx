1c8ab676ee5bd80f6507b53a064678e70d205e7f
ejim11 <favourejim56@gmail.com>
2025-05-19 00:41:58 +0100

    🎨 added metadata for the product details page

--- app/[productSlug]/page.tsx
+++ app/[productSlug]/page.tsx
@@ -1,9 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use client";
 import ProductDetail from "@/components/ProductDetail";
+import products, { Product } from "@/data/products";
+import { Metadata } from "next";
 import React, { Usable } from "react";
 
-export default function Product({
+type Params = Promise<{ productSlug: string }>;
+
+// Utility function to fetch product data
+function fetchProduct(productSlug: string) {
+  const product: Product = products.filter(
+    (product: Product) => product.slug.toLowerCase() === productSlug
+  )[0];
+
+  return product;
+}
+
+// Async function to generate dynamic metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { productSlug } = await params;
+  // Fetch product details
+  const product = fetchProduct(productSlug);
+
+  return {
+    title: `AI Power Bundle | ${product.title}`,
+    description: `${product.title} - ${product.details[0]}`,
+  };
+}
+
+export default function ProductDetailPage({
   params,
 }: Usable<{ productSlug: string }> | any) {
   const slug: { productSlug: string } = React.use(params);
